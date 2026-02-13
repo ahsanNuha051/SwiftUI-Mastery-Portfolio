9b0c698d6c7d7dd30f0292227dd3cfbbc10f1bdf
ahsanNuha051 <ahsana051@gmail.com>
2026-02-13 19:49:03 +0500

    Add App 01 - Hiya SwiftUI Basics

---
+++ Hiya/Hiya/HiyaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HiyaApp.swift
+//  Hiya
+//
+//  Created by Ahsan Rizvi on 13/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct HiyaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
