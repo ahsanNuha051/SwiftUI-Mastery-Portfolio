d69c2cfb006303c3339af1926814a2e844c265af
ahsanNuha051 <ahsana051@gmail.com>
2026-02-19 12:13:26 +0500

    Hiya App, Completed Project setup, Ai avialabity check

--- Hiya/Hiya/ContentView.swift
+++ Hiya/Hiya/ContentView.swift
@@ -6,14 +6,27 @@
 //
 
 import SwiftUI
+import FoundationModels
 
 struct ContentView: View {
+    private var largeLanguageModel = SystemLanguageModel.default
+       
+    @State private var response: String = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            switch largeLanguageModel.availability {
+            case .available:
+                Text(response)
+            case .unavailable(.deviceNotEligible):
+              Text("Your device isn't eligible for Apple Intelligence.")
+            case .unavailable(.appleIntelligenceNotEnabled):
+        Text("Apple Intelligence isn't enabled, Please enable it in Settings.")
+            case .unavailable(.modelNotReady):
+                Text("AI Model not ready.")
+            case .unavailable(_):
+                Text("AI feature is unavailable for unknown reason.")
+            }
         }
         .padding()
     }
