1fed60ffc304c5022ff66ae4e610eafdfd73049a
ahsanNuha051 <ahsana051@gmail.com>
2026-02-19 13:10:40 +0500

    AI language model session

--- Hiya/Hiya/ContentView.swift
+++ Hiya/Hiya/ContentView.swift
@@ -10,25 +10,54 @@ import FoundationModels
 
 struct ContentView: View {
     private var largeLanguageModel = SystemLanguageModel.default
-       
+    private var session = LanguageModelSession()
+    
     @State private var response: String = ""
     
     var body: some View {
         VStack {
+            Spacer()
             switch largeLanguageModel.availability {
             case .available:
                 Text(response)
+                    .multilineTextAlignment(.center)
+                    .font(.largeTitle)
+                    .bold()
             case .unavailable(.deviceNotEligible):
-              Text("Your device isn't eligible for Apple Intelligence.")
+                Text("Your device isn't eligible for Apple Intelligence.")
             case .unavailable(.appleIntelligenceNotEnabled):
-        Text("Apple Intelligence isn't enabled, Please enable it in Settings.")
+                Text("Apple Intelligence isn't enabled, Please enable it in Settings.")
             case .unavailable(.modelNotReady):
                 Text("AI Model not ready.")
             case .unavailable(_):
                 Text("AI feature is unavailable for unknown reason.")
             }
+            Spacer()
+            
+            Button {
+                // Action
+                Task{
+                    let prompt = "Hello, how can I help you today?"
+                    do {
+                        let replay = try await session.respond(to: prompt)
+                        response = replay.content
+                    } catch {
+                        response = "Failed to get response: \(error.localizedDescription)"
+                    }
+                }
+            } label: {
+                Text("Welcome")
+                    .font(.largeTitle)
+                    .padding()
+            }
+            .buttonStyle(.borderedProminent)
+            .buttonSizing(.flexible)
+            .glassEffect(.regular.interactive())
+            
+            
         }
         .padding()
+        .tint(.purple)
     }
 }
 
