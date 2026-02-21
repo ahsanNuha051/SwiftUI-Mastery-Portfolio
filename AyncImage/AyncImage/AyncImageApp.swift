27b3a7f7c16b2a091b71b57636b9a6b59aef622a
ahsanNuha051 <ahsana051@gmail.com>
2026-02-22 01:08:56 +0500

    AsyncImage remote internet image SwiftUI App

---
+++ AyncImage/AyncImage/AyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  AyncImageApp.swift
+//  AyncImage
+//
+//  Created by Ahsan Rizvi on 22/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct AyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
